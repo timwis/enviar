168cd7c7c3b65d084b308260502b0af59c392371
timwis <tim@timwis.com>
2016-08-19 07:18:05 -0400

    reformat .couchdb messages filter to be more node-module-like

--- .couchdb/messages/index.js
+++ .couchdb/messages/index.js
@@ -1,6 +1,8 @@
-exports._id = '_design/messages'
-exports.filters = {
-  'pending-outbound': function (doc, req) {
-    return !doc.providerId && doc.direction === 'outbound'
+module.exports = {
+  _id: '_design/messages',
+  filters: {
+    'pending-outbound': function (doc, req) {
+      return !doc.providerId && doc.direction === 'outbound'
+    }
   }
 }

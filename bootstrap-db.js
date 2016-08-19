8d600bbd705d512709bfc4edc85cf6c80976c598
timwis <tim@timwis.com>
2016-08-19 08:06:51 -0400

    bootstrap creates database

--- bootstrap-db.js
+++ bootstrap-db.js
@@ -4,15 +4,20 @@ const dbName = 'messages'
 
 const push = require('couchdb-push')
 const configure = require('couchdb-configure')
+const ensure = require('couchdb-ensure')
 
 const couchPath = couchdb + '/' + dbName
-console.log(couchPath)
-push(couchPath, './.couchdb/messages', { index: true }, (err, resp) => {
-  if (err) return console.error(err)
-  console.log(resp)
-})
 
-configure(couchdb, './.couchdb/_config.json', (err, resp) => {
-  if (err) return console.error(err)
-  console.log(resp)
+ensure(couchPath, (err, resp) => {
+  if (err) return console.error('Error creating database', err)
+
+  configure(couchdb, './.couchdb/_config.json', (err, resp) => {
+    if (err) return console.error('Error configuring database', err)
+    console.log(resp)
+  })
+
+  push(couchPath, './.couchdb/messages', { index: true }, (err, resp) => {
+    if (err) return console.error('Error creating design document', err)
+    console.log(resp)
+  })
 })

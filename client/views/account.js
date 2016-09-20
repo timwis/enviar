a02df16115b1048e920840617deac7a4491fde81
timwis <tim@timwis.com>
2016-09-20 07:45:52 -0400

    side panel and account page should reflect that usernames are emails

--- client/views/account.js
+++ client/views/account.js
@@ -11,15 +11,19 @@ const prefix = css`
   #logout {
     width: 200px;
   }
+  #current-login {
+    margin-top: 0;
+  }
 `
 
 module.exports = (state, prev, send) => {
   return html`
     <section class=${prefix}>
       <h2>Logout</h2>
+      <p id="current-login">Logged in as ${state.user.name}</p>
       <button onclick=${onClickLogout} class="pure-button pure-button-primary" id="logout">
         <i class="fa fa-sign-out"></i>
-        Logout as ${state.user.name}
+        Logout
       </button>
 
       <h2>Change password</h2>
@@ -36,20 +40,6 @@ module.exports = (state, prev, send) => {
 
         <button type="submit" class="pure-button pure-button-primary">Change Password</button>
       </form>
-
-      <h2>Change email</h2>
-      <form class="pure-form pure-form-stacked" onsubmit=${onChangeEmail}>
-        <label>
-          New Email
-          <input type="email" name="email">
-        </label>
-
-        <button type="submit" class="pure-button pure-button-primary">Change Email</button>
-
-        ${state.ui.changeEmailSubmitted
-          ? html`<div class="alert">Your email has been changed.</div>`
-          : ''}
-      </form>
     </section>`
 
   function onChangePassword (e) {
@@ -58,16 +48,6 @@ module.exports = (state, prev, send) => {
       send('user:changePassword', formData)
     } else {
       console.error('Passwords do not match')
-    }
-    e.preventDefault()
-  }
-
-  function onChangeEmail (e) {
-    const input = e.target.querySelector('[name=email]')
-    const email = input.value
-    if (email) {
-      send('user:changeEmail', {email})
-      input.value = ''
     }
     e.preventDefault()
   }
